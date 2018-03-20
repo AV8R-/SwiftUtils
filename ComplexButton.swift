a7b312ff7a763794baaee1a3cb82009a7b680e38
Bogdan Manshilin <skimphiter@gmail.com>
2018-03-20 23:47:14 +0200

    fix: crash when complex button is empty

--- ComplexButton.swift
+++ ComplexButton.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class ComplexButton: UIControl {
+public class ComplexButton: UIControl {
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initOverlayButton()
     }
@@ -30,9 +30,14 @@ class ComplexButton: UIControl {
         }
     }
     
+    public var shouldMakeTranclucentOnHiglhlight = true
+    
     lazy var nestedButtons: [UIButton] = {
         var buttons = [UIButton]()
-        let count = self.subviews.count - 1 > 0 ? self.subviews.count - 1 : 0
+        let count = self.subviews.count - 1
+        guard count > 0 else {
+            return []
+        }
         let sbviews = Array(self.subviews[0..<1])
         self.recursive(subviews: sbviews) {
             if let button = $0 as? UIButton {
@@ -56,15 +61,18 @@ class ComplexButton: UIControl {
         self.overlay = overlay
     }
     
-    override var isEnabled: Bool { didSet {
+    override public var isEnabled: Bool { didSet {
         self.nestedButtons.forEach { $0.isEnabled = isEnabled }
     }}
 
-    override var isSelected: Bool { didSet {
+    override public var isSelected: Bool { didSet {
         self.nestedButtons.forEach { $0.isSelected = isSelected }
     }}
 
-    override var isHighlighted: Bool { didSet {
+    override public var isHighlighted: Bool { didSet {
+        guard shouldMakeTranclucentOnHiglhlight else {
+            return
+        }
         let count = subviews.count - 1 > 0 ? subviews.count - 1 : 0
         let alpha: CGFloat = isHighlighted ? 0.3 : 1
         for subview in subviews[0..<count] {
@@ -72,12 +80,12 @@ class ComplexButton: UIControl {
         }
     }}
     
-    override func layoutIfNeeded() {
+    override public func layoutIfNeeded() {
         super.layoutIfNeeded()
         self.bringSubview(toFront: overlay)
     }
     
-    override func addSubview(_ view: UIView) {
+    override public func addSubview(_ view: UIView) {
         if let overlay = overlay {
             insertSubview(view, belowSubview: overlay)
         } else {
@@ -85,44 +93,44 @@ class ComplexButton: UIControl {
         }
     }
     
-    override var contentVerticalAlignment: UIControlContentVerticalAlignment { didSet {
+    override public var contentVerticalAlignment: UIControlContentVerticalAlignment { didSet {
         self.nestedButtons.forEach { $0.contentVerticalAlignment = contentVerticalAlignment }
     }}
     
-    override var contentHorizontalAlignment: UIControlContentHorizontalAlignment{ didSet {
+    override public var contentHorizontalAlignment: UIControlContentHorizontalAlignment{ didSet {
         self.nestedButtons.forEach { $0.contentHorizontalAlignment = contentHorizontalAlignment }
     }}
     
     
-    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+    override public func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         self.nestedButtons.forEach { $0.beginTracking(touch, with: event) }
         return super.beginTracking(touch, with: event)
     }
 
-    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+    override public func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         self.nestedButtons.forEach { $0.continueTracking(touch, with: event) }
         return super.continueTracking(touch, with: event)
     }
 
-    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+    override public func endTracking(_ touch: UITouch?, with event: UIEvent?) {
         self.nestedButtons.forEach { $0.endTracking(touch, with: event) }
         return super.endTracking(touch, with: event)
     }
 
-    override func cancelTracking(with event: UIEvent?) {
+    override public func cancelTracking(with event: UIEvent?) {
         self.nestedButtons.forEach { $0.cancelTracking(with: event) }
         super.cancelTracking(with: event)
     }
     
-    override func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControlEvents) {
+    override public func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControlEvents) {
         overlay.addTarget(target, action: action, for: controlEvents)
     }
     
-    override func removeTarget(_ target: Any?, action: Selector?, for controlEvents: UIControlEvents) {
+    override public func removeTarget(_ target: Any?, action: Selector?, for controlEvents: UIControlEvents) {
         overlay.removeTarget(target, action: action, for: controlEvents)
     }
     
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    override public func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         let newValue = change![.newKey] as! Bool
         switch keyPath {
         case "highlighted"?: isHighlighted = newValue
